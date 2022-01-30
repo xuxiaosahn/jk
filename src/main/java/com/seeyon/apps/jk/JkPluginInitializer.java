59641398d1ba1795700d5db9f30a552fc2e8d36d
许小山 <2543607510@qq.com>
2022-01-30 23:22:01 +0800

    忽略target文件夹，删除默认index.jsp; 增加mysql脚本以及自动建表工具类

--- src/main/java/com/seeyon/apps/jk/JkPluginInitializer.java
+++ src/main/java/com/seeyon/apps/jk/JkPluginInitializer.java
@@ -1,18 +1,19 @@
 package com.seeyon.apps.jk;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.Logger;
-
+import com.seeyon.apps.jk.util.ExcuteSqlFileUtil;
 import com.seeyon.ctp.common.AbstractSystemInitializer;
 import com.seeyon.ctp.common.plugin.PluginDefinition;
 import com.seeyon.ctp.common.plugin.PluginInitializer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 
 public class JkPluginInitializer extends AbstractSystemInitializer implements PluginInitializer{
 	private static Log LOG = LogFactory.getLog(JkPluginInitializer.class);
 	@Override
 	public void initialize() {
 		LOG.info("初始化quartz管理插件");
+		ExcuteSqlFileUtil.initSqlFile();
 	}
 	@Override
 	public void destroy() {
